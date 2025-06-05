12660672f24d3b91bc7093ec070dc0595921fc00
vinidotruan <contatoruanvinicius@gmail.com>
2025-06-05 11:09:08 -0300

    small code smells 🐀

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -31,6 +32,15 @@ type Game struct {
 
 func (s *Square) Draw() {
 	rl.DrawRectangleV(s.Position, s.Size, s.Color)
+	value := fmt.Sprint(s.Value)
+	textPosition := func () (int32, int32) {
+		X := s.Position.X + s.Size.X / 2
+		Y := s.Position.Y + s.Size.Y / 2
+
+		return int32(X), int32(Y)
+	}
+	x, y := textPosition()
+	rl.DrawText(value, x, y, 10, rl.White)
 }
 
 func (g *Game) NewSquare(x int32, y int32) {
@@ -70,14 +80,24 @@ func main() {
 	g.NewSquare(600, 400)
 
 	for !rl.WindowShouldClose() {
-
-
 		rl.BeginDrawing()
+		
+		g.Update()
+		g.Draw()
 
+		rl.ClearBackground(background)
+		rl.EndDrawing()
+	}
+
+}
+
+func (g *Game) Draw() {
+		// Draw squares
 		for i := 0; i < len(g.Squares); i++ {
 			g.Squares[i].Draw()
 		}
 
+		// Draw grid
 		for x := int32(0); x < int32(width); x += tileWidth {
 			rl.DrawLine(x, 0, x, height, rl.White)
 		}
@@ -85,79 +105,44 @@ func main() {
 		for y := int32(0); y < int32(height); y += tileHeight {
 			rl.DrawLine(0, y, width, y, rl.White)
 		}
+}
 
-		if rl.IsKeyPressed(rl.KeyUp) {
-			newSquarePosX := randRange(0, 4) * tileWidth
-			newSquarePosY := randRange(0, 4) * tileHeight
+func (g *Game) Update() {
+	g.MovimentHandle()
+}
 
-			for i := 0; i < len(g.Squares); i++ {
-				maxValue := 0
-				// TODO: checar a colisao de todos
-				g.Squares[i].Position.Y = float32(maxValue)
-			}
-
-			// TODO: provavelmente ainda terao coordenadas duplicadas
-			for i := 0; i < len(g.Squares); i++ {
-				if(newSquarePosY == int32(g.Squares[i].Position.Y)) {
-					newSquarePosY = randRange(0, 4) * tileWidth
-				}
-			}
-			
-			g.NewSquare(int32(newSquarePosX), int32(newSquarePosY))
-		}
-
+func (g *Game) MovimentHandle() {
 		if rl.IsKeyPressed(rl.KeyDown) {
 			for i := 0; i < len(g.Squares); i++ {
 				maxValue := tileHeight * 3
 				g.Squares[i].Position.Y = float32(maxValue)
 			}
-			
+			g.GenerateNewSquare()
+		}
+
+		if rl.IsKeyPressed(rl.KeyUp) {
+			for i := 0; i < len(g.Squares); i++ {
+				maxValue :=  0
+				g.Squares[i].Position.Y = float32(maxValue)
+			}
+			g.GenerateNewSquare()
+		}
+
+		if rl.IsKeyPressed(rl.KeyLeft) {
+			for i := 0; i < len(g.Squares); i++ {
+				maxValue := 0
+				g.Squares[i].Position.X = float32(maxValue)
+			}
 			g.GenerateNewSquare()
 		}
 
 		if rl.IsKeyPressed(rl.KeyRight) {
-			newSquarePosX := randRange(0, 4) * tileWidth
-			newSquarePosY := randRange(0, 4) * tileHeight
-
 			for i := 0; i < len(g.Squares); i++ {
-				maxValue := tileWidth * 3
-				// TODO: checar a colisao de todos
+				maxValue :=  tileWidth * 3
 				g.Squares[i].Position.X = float32(maxValue)
 			}
-
-			// TODO: provavelmente ainda terao coordenadas duplicadas
-			for i := 0; i < len(g.Squares); i++ {
-				if(newSquarePosY == int32(g.Squares[i].Position.Y)) {
-					newSquarePosY = randRange(0, 4) * tileWidth
-				}
-			}
-			
-			g.NewSquare(int32(newSquarePosX), int32(newSquarePosY))
+			g.GenerateNewSquare()
 		}
-
-		if rl.IsKeyPressed(rl.KeyLeft) {
-			newSquarePosX := randRange(0, 4) * tileWidth
-			newSquarePosY := randRange(0, 4) * tileHeight
-
-			for i := 0; i < len(g.Squares); i++ {
-				maxValue := 0
-				// TODO: checar a colisao de todos
-				g.Squares[i].Position.X = float32(maxValue)
-			}
-
-			// TODO: provavelmente ainda terao coordenadas duplicadas
-			for i := 0; i < len(g.Squares); i++ {
-				if(newSquarePosY == int32(g.Squares[i].Position.Y)) {
-					newSquarePosY = randRange(0, 4) * tileWidth
-				}
-			}
-			
-			g.NewSquare(int32(newSquarePosX), int32(newSquarePosY))
-		}
-		rl.ClearBackground(background)
-		rl.EndDrawing()
-	}
-
 }
 
 func randRange(min, max int) int32 {
